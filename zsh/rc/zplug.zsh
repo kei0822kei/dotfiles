d5b7d0bd8f1d0593a7d538b05e5dd9d27e2ec789
kei0822kei <kei0822kei.football@gmail.com>
2018-07-07 19:38:34 +0900

    datat

---
+++ zsh/rc/zplug.zsh
@@ -0,0 +1,39 @@
+#!/usr/bin/zsh
+
+### set ZPLUG_HOME
+export ZPLUG_HOME="$HOME/.mydot/zsh/.zplug"
+
+### zplug install
+if [ ! -e "$ZDOTDIR/.zplug" ]; then
+  git clone https://github.com/zplug/zplug $ZPLUG_HOME
+fi
+
+### zplug setting
+source $ZPLUG_HOME/init.zsh
+
+### install plugins
+zplug "mollifier/anyframe"                    # like denite.nvim
+zplug "sorin-ionescu/prezto"
+#zplug "b4b4r07/enhancd", at:v1                # at: はブランチとタグをサポートしている
+zplug "junegunn/fzf-bin", from:gh-r, as:command, rename-to:"fzf"
+zplug "zsh-users/zsh-autosuggestions"         # auto completion
+zplug "zsh-users/zsh-completions"             # complete sub commands  ex) git <TAB>
+zplug "zsh-users/zsh-syntax-highlighting"     # color
+zplug "zsh-users/zaw"     # color
+# zplug "bhilburn/powerlevel9k", use:"powerlevel9k.zsh-theme", as:theme
+#zplug "sindresorhus/pure"     # color
+#zplug "zsh-prompt-powerline"
+
+#zplug 'jhawthorn/fzy'
+
+if ! zplug check --verbose; then
+  printf 'Install? [y/N]: '
+  if read -q; then
+    echo; zplug install
+  fi
+fi
+
+zplug load --verbose
+
+### setting of each plugin
+source "$ZDOTDIR/rc/anyframe.zsh"
