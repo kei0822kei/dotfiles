627ce9147a2fdbe6b70ac13f6c10d91065af3bcd
kei0822kei <mizokami.keiyu@gmail.com>
2019-10-24 11:26:40 +0900

    gh-pages

--- tmux/ssh-change-profile.bash
+++ tmux/ssh-change-profile.bash
@@ -4,7 +4,8 @@
 echo -ne "\033]1337;SetProfile=$@\a"
  
 # ssh login
-/usr/bin/ssh -X "$@"
+# /usr/bin/ssh -X "$@"
+/usr/bin/ssh -X -Y "$@"
 
 # set profile(default)
 echo -ne "\033]1337;SetProfile=Default\a"
