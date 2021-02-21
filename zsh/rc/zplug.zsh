205972d1731d8affd6d390db71e4af45f9e603fe
kei0822kei <mizokami.keiyu@gmail.com>
2021-02-21 21:37:47 +0900

    Add: Remap :q to c

--- zsh/rc/zplug.zsh
+++ zsh/rc/zplug.zsh
@@ -13,13 +13,13 @@ source $ZPLUG_HOME/init.zsh
 
 ### install plugins
 zplug "mollifier/anyframe"                    # like denite.nvim
-zplug "sorin-ionescu/prezto"
+# zplug "sorin-ionescu/prezto"
 # zplug "b4b4r07/enhancd", at:v1                # at: はブランチとタグをサポートしている
 zplug "junegunn/fzf-bin", from:gh-r, as:command, rename-to:"fzf"
 zplug "zsh-users/zsh-autosuggestions"         # auto completion
 zplug "zsh-users/zsh-completions"             # complete sub commands  ex) git <TAB>
 zplug "zsh-users/zsh-syntax-highlighting"     # color
-zplug "zsh-users/zaw"     # color
+# zplug "zsh-users/zaw"     # color
 # zplug "bhilburn/powerlevel9k", use:powerlevel9k.zsh-theme
 # zplug "bhilburn/powerlevel9k", use:"powerlevel9k.zsh-theme", as:theme
 #zplug "sindresorhus/pure"     # color
